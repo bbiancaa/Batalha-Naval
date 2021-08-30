6a2d9ad609b4b8b8be8051670f6196e906e8812a
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-08-30 09:50:24 -0300

    adicionando comentarios

--- battle_ship.asm
+++ battle_ship.asm
@@ -8,24 +8,24 @@ columns:	.word 10
 
 .text
 main:
-	la	a1, ships			# lê endereço do vetor
+	la	a1, ships				# lê endereço do vetor
 	la	s0, lines
 	la 	s1, columns
 	add	s3, s3, zero
 	
-	lbu  	s2, (a1)			# pego valor primeira posicao = quantidade de navios	
-	addi	a1, a1, 2			# pula 2 caracter pois informacao necessaria ja adqurida na primeira linha
+	lbu  	s2, (a1)				# pego valor primeira posicao = quantidade de navios	
+	addi	a1, a1, 2				# pula 2 caracter pois informacao necessaria ja adqurida na primeira linha
 	
-	li  	t1, '\n'			# grava \n
-	li  	t2, '\0'			# grava eof
-	li  	t3, '0'				# horizontal
-	li  	t4, '1'				# vertical
-	j	insere_embarcacoes		# chama função insere_embarcacoes:
+	li  	t1, '\n'				# grava \n
+	li  	t2, '\0'				# grava eof
+	li  	t3, '0'					# horizontal
+	li  	t4, '1'					# vertical
+	j	insere_embarcacoes			# chama função insere_embarcacoes:
 
 
 insere_embarcacoes:  
 
-	lbu  	t0, (a1)			# estende endereco de memoria atual de a1 para t0
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	
 	beq	t0, t3, insere_na_horizontal		# insere navio na horizontal
 	beq	t0, t4, insere_na_vertical		# insere navio na vertical
@@ -33,38 +33,36 @@ insere_embarcacoes:
 
 
 loop_find_eof:
-	lbu  	t0, (a1)			# estende endereco de memoria atual de a1 para t0
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	j	insere_embarcacoes
 	
 	
 
 insere_na_horizontal:
-	beq   	t0, t1, loop_find_eof 		# verifica se possui \n para proxima interacao
 	jal	loop_all_caracters
 
-	j insere_na_horizontal
+	j insere_na_horizontal				# funcao recursiva para percorrer toda a linha
 	
 
 insere_na_vertical:
 
-	beq   	t0, t1, loop_find_eof 		# verifica se possui \n para proxima interacao
 	jal	loop_all_caracters
 	
-	mv 	a0, t0  			# imprime para ver qual é a posicao atual
+	mv 	a0, t0  				# imprime para ver qual é a posicao atual
 	li 	a7, 11
 	ecall	
 	
-	j insere_na_vertical
+	j insere_na_vertical				# funcao recursiva para percorrer toda a linha
 	
 loop_all_caracters:
 
-	lbu  	t0, (a1)			# estende endereco de memoria atual de a1 para t0
-	addi 	a1, a1, 1			# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	
 	
-	beq   	t0, t1, loop_find_eof 		# verifica se possui \n para proxima interacao
+	beq   	t0, t1, loop_find_eof 			# verifica se possui \n para proxima interacao
 	
-	beq   	t0, t2, fim 			# verifica se endereço atual é \0 e encerra for
+	beq   	t0, t2, fim 				# verifica se endereço atual é \0 e encerra for
 	
 	ret
 	
