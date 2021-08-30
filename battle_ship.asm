cbea56efd3765490fc76adcaa6258e87080bf808
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-08-30 07:27:34 -0300

    printando char para exibir posicao e salvando tamanho do for

--- battle_ship.asm
+++ battle_ship.asm
@@ -8,16 +8,22 @@ columns:	.word 10
 
 .text
 main:
-	la	a1, ships			# lê endereço do vetor
+	la	a1, ships			# lÃª endereÃ§o do vetor
 	la	s0, lines
 	la 	s1, columns
+	
+	lbu  	s2, (a1)			# pego valor primeira posicao = quantidade de navios	
+	addi	a1, a1, 2			# pula 2 caracter pois informacao necessaria ja adqurida na primeira linha
+	
 	li  	t1, '\n'			# grava \n
 	li  	t2, '\0'			# grava eof
-	jal	insere_embarcacoes		# chama função insere_embarcacoes:
+	jal	insere_embarcacoes		# chama funÃ§Ã£o insere_embarcacoes:
 
 
 insere_embarcacoes:  
+
 	beq   	t0, t1, loop_find_eof 		# verifica se possui \n para proxima interacao
+	
 	jal	loop_all_caracters		# percorre toda a string
 	
 
@@ -25,23 +31,21 @@ insere_embarcacoes:
 
 loop_find_eof:
 
-	la      a0, n
-	li 	a7, 4
-	ecall	
 	lbu  	t0, (a1)			# estende endereco de memoria atual de a1 para t0
 	jal insere_embarcacoes			# volta para inserir embarcacoes
 	
 loop_all_caracters:
+
 	lbu  	t0, (a1)			# estende endereco de memoria atual de a1 para t0
 	addi 	a1, a1, 1			# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	
+	mv 	a0, t0  			# imprime para ver qual Ã© a posicao atual
+	li 	a7, 11
+	ecall	
 	
 	addi 	s9, a1, 1			# verifica se esta no final da string \0 para evitar loop infinito
 	lbu  	s10, (s9)
 	beq   	s10, t2, fim 
-	
-	mv 	a0, a1  			# imprime para ver como esta percorrendo
-	li 	a7, 4
-	ecall	
 	
 	jal insere_embarcacoes			#se nao continua percorrendo 
 fim:
