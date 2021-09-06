835fd4a6f9f9d06aecbf727299ab761eea2f0230
Bianca Gabriela Fritsch <biancagabriela.fritsch@hotmail.com>
2021-09-06 15:21:10 -0300

    Update battle_ship.asm

--- battle_ship.asm
+++ battle_ship.asm
@@ -1,9 +1,7 @@
 .data
 
 ships:							.asciz "3\n1511\n0522\n0164"
-n:							.asciz "\n"	
-lines:							.word 10
-columns:						.word 10
+n:							.asciz "\n"
 matriz:							.word 100 #pensando em uma maneira diferente de salvar os valores
 
 .text
@@ -11,11 +9,11 @@ main:
 	la	a1, ships				# lê endereço do vetor
 	la	s0, matriz				# le matriz para ser escrita
 	la	s8, matriz				# le matriz para ser printada
-	la 	s1, columns
 	add	s3, s3, zero				# autoincrement para for de printar
 	addi	s4, s4, 10				# valor para adicionar \n
 	addi	s5, s5, 100				# tamanho da matriz
 	
+	add	s1, s1, zero				# contador para linha
 	lbu  	s2, (a1)				# pego valor primeira posicao = quantidade de navios	
 	addi	a1, a1, 2				# pula 2 caracter pois informacao necessaria ja adqurida na primeira linha
 	
@@ -29,7 +27,6 @@ main:
 insere_embarcacoes:  
 
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
-	
 	beq	t0, t3, insere_na_horizontal		# insere navio na horizontal
 	beq	t0, t4, insere_na_vertical		# insere navio na vertical
 	j	loop_matriz
@@ -37,12 +34,30 @@ insere_embarcacoes:
 
 
 loop_find_eof:
+	add	s1, s1, zero				# reseto contador para linha
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	j	insere_embarcacoes
 	
 	
 
 insere_na_horizontal:
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a2, a2, zero				# tamanho do navio
+	addi 	a2, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
+	
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a3, a3, zero				# linha do navio
+	addi	a3, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
+	
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a4, a4, zero				# coluna do navio
+	addi	a4, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
 	jal	loop_all_caracters
 	jal	preenche_vetor_horizontal
 	j 	insere_na_horizontal			# funcao recursiva para percorrer toda a linha
@@ -50,10 +65,28 @@ insere_na_horizontal:
 
 insere_na_vertical:
 
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a2, a2, zero				# tamanho do navio
+	addi 	a2, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
+	
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a3, a3, zero				# linha do navio
+	addi	a3, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
+	
+	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
+	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
+	add	a4, a4, zero				# coluna do navio
+	addi	a4, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	
+	
 	jal	loop_all_caracters
 	jal	preenche_vetor_vertical
 	
-	j insere_na_vertical				# funcao recursiva para percorrer toda a linha
+	j 	insere_na_vertical			# funcao recursiva para percorrer toda a linha
 	
 loop_all_caracters:
 
@@ -63,7 +96,7 @@ loop_all_caracters:
 	
 	beq   	t0, t1, loop_find_eof 			# verifica se possui \n para proxima interacao
 	
-	beq   	t0, t2, print_n 			# verifica se endereço atual é \0 vai printar a matriz
+	beq   	t0, t2, loop_matriz 			# verifica se endereço atual é \0 vai printar a matriz
 	
 	ret
 
@@ -78,10 +111,12 @@ preenche_vetor_horizontal:
 	
 	ret
 
+
+
 loop_matriz:
-	lw	a4, (s8)				# carregando matriz de endereços copiada
+	lw	a6, (s8)				# carregando matriz de endereços copiada
 	
-	mv 	a0, a4  				# imprime os valores
+	mv 	a0, a6  				# imprime os valores
 	li 	a7, 11
 	ecall	
 	
@@ -103,7 +138,8 @@ print_n:
 	ecall	
 	j 	loop_matriz				#volto pra matriz
 	
+	
+	
 fim:
 
 	nop
-	
