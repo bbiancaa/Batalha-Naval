e5afd266989552673a8f3f7a8c83720b7e3b40f5
Bianca Gabriela Fritsch <biancagabriela.fritsch@hotmail.com>
2021-09-06 18:58:28 -0300

    Update battle_ship.asm

--- battle_ship.asm
+++ battle_ship.asm
@@ -1,7 +1,7 @@
 .data
 
 ships:							.asciz "3\n1511\n0522\n0164"
-columns:							.asciz "0 1 2 3 4 5 6 7 8 9\n"
+columns:						.asciz "0 1 2 3 4 5 6 7 8 9\n"
 lines:							.asciz "0\n1\n2\n3\n4\n5\n6\n7\n8\n9"
 n:							.asciz "\n"
 matriz:							.word 100 #pensando em uma maneira diferente de salvar os valores
@@ -9,8 +9,6 @@ matriz:							.word 100 #pensando em uma maneira diferente de salvar os valores
 .text
 main:
 	la	a1, ships				# lê endereço do vetor
-	la	s0, matriz				# le matriz para ser escrita
-	la	s8, matriz				# le matriz para ser printada
 	la	s7, columns				# le matriz para ser printada
 	add	s3, s3, zero				# autoincrement para for de printar
 	addi	s4, s4, 10				# valor para adicionar \n
@@ -58,13 +56,13 @@ insere_na_horizontal:
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	add	a3, a3, zero				# linha do navio
-	addi	a3, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	addi	a3, t0, -48				# coloca a linha inicial do navio lido na linha converte pra int e grava
 	
 	
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	add	a4, a4, zero				# coluna do navio
-	addi	a4, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	addi	a4, t0, -48				# coloca o coluna inicial do navio lido na linha converte pra int e grava
 	
 	mul	t5, a3, s4				# multiplico pra ter a coluna inicial
 	add	a5, t5, a4				# somo a coluna com a linha pra saber posicao na matriz
@@ -85,13 +83,13 @@ insere_na_vertical:
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	add	a3, a3, zero				# linha do navio
-	addi	a3, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	addi	a3, t0, -48				# coloca o linha inicial do navio lido na linha converte pra int e grava
 	
 	
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	add	a4, a4, zero				# coluna do navio
-	addi	a4, t0, -48				# coloca o tamanho do navio lido na linha converte pra int e grava
+	addi	a4, t0, -48				# coloca o coluna inicial do navio lido na linha converte pra int e grava
 	
 	
 	mul	t5, a3, s4				# multiplico pra ter a coluna inicial
@@ -141,7 +139,7 @@ header:
 	
 loop_matriz:
 	beqz	s3, header
-	lw	a6, (s8)				# carregando matriz de endereços copiada
+	lw	a6, (s9)				# carregando matriz de endereços copiada
 	
 	mv 	a0, a6  				# imprime os valores
 	li 	a7, 11
@@ -151,7 +149,7 @@ loop_matriz:
 	li 	a7, 11
 	ecall	
 	
-	addi	s8, s8, 4				# vou para proxima posicao do vetor copiado
+	addi	s9, s9, 4				# vou para proxima posicao do vetor copiado
 	addi	s3, s3, 1				# autoincremento meu for
 	remu	s6, s3, s4				# pego resto da divisão do autoincrementro / 10
 	beq	s3, s5, fim				# vejo se o for precisa ser parado
