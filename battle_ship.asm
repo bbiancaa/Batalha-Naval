00b416af6aca8fdfd9c68018e04525a4335832f8
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-20 22:11:31 -0300

    Update battle_ship.asm

--- battle_ship.asm
+++ battle_ship.asm
@@ -1,6 +1,7 @@
 .data
 
-ships:							.asciz 	"3 \n 1 5 1 1 \n 0 5 2 2 \n 0 1 6 4 "
+ships:							.asciz 	"3 \n 1 5 1 1 \n 0 5 2 2 \n 0 1 6 4"
+error:							.asciz 	"Entrada invalida"
 columns:						.asciz 	"0 1 2 3 4 5 6 7 8 9\n"
 lines:							.asciz 	"0\n1\n2\n3\n4\n5\n6\n7\n8\n9"
 n:							.asciz 	"\n"
@@ -32,7 +33,8 @@ verifica_vazio:
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, aqui por ser string cada posicao tem 8 bits
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	
-	bne 	t0, a6, fim
+	bne 	t0, a6, fim_error
+	beq   	t0, t2, loop_matriz 			# verifica se endereço atual é \0 vai printar a matriz
 	addi 	a1, a1, 1				# pula 1 endereco de memoria, se for espaco na posicao atual
 	lbu  	t0, (a1)				# estende endereco de memoria atual de a1 para t0
 	ret
@@ -163,7 +165,13 @@ print_n:
 	ecall	
 	j 	loop_matriz				#volto pra matriz
 	
-	
+		
+fim_error:
+	la	s7, error				# le matriz para ser printada
+	mv 	a0, s7  				# imprime os valores
+	li 	a7, 4
+	ecall	
+	nop
 	
 fim:
 	nop
