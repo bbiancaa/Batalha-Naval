dad51ed9cf13d3e1847ab38cd9f5704f883d403c
DIOGO BALTAZAR DO NASCIMENTO <diogoblibi@hotmail.com>
2021-09-27 10:49:09 -0300

    adicionando procura de navios caso tiro acerte algum navio

--- battle_ship.asm
+++ battle_ship.asm
@@ -4,11 +4,13 @@ ships:							.asciz 	"3 \n 1 5 1 1 \n 0 5 2 2 \n 0 1 6 4 "
 error:							.asciz 	"Entrada invalida"
 error_sobreposicao:					.asciz  "Sobreposição nos navios"
 error_tam:						.asciz  "O navio extrapola as dimensões da matriz"
+error_tiro:						.asciz  "\n\nVoce errou seu tiro\n"
+acertou_navio:						.asciz  "\n\nVoce acertou um navio\n"
 columns:						.asciz 	"0 1 2 3 4 5 6 7 8 9\n"
 msg_Linha:						.asciz  "Digite o numero da linha pro tiro: "
 msg_Coluna:						.asciz  "Digite o numero da coluna pro tiro: "
-matriz_jogo:						.word 	100 # matriz q vai aparecer durante jogo
 matriz:							.word 	100 # matriz q vai ser escrita os navios
+matriz_jogo:						.word 	100 # matriz q vai aparecer durante jogo
 	
 .text
 main:
@@ -55,7 +57,7 @@ insere_embarcacoes:
 	
 	add	s1, zero, zero				# reseto contador para linha
 	beq   	t0, t1, loop_find_eof 			# verifica se possui \n para proxima interacao
-	beq   	t0, t2, jogo			# verifica se endereço atual é \0 vai printar a matriz
+	beq   	t0, t2, inicia_game			# verifica se endereço atual é \0 vai printar a matriz
 	beq	t0, t3, insere_na_horizontal		# insere navio na horizontal
 	beq	t0, t4, insere_na_vertical		# insere navio na vertical
 	
@@ -199,7 +201,7 @@ preenche_vetor_jogo:
 	add	s11, zero, zero
 	addi	s11, zero, 42				# valor padrao da matriz vai ser X
 	
-	sw	s11, 0(s8)
+	sw	s11, 400, (s8)
 	addi	s8, s8, 4				# escrevo valor padrao em todas as posicoes
 	beq	t2, s5, inicia_game
 	j 	preenche_vetor_jogo
@@ -223,9 +225,8 @@ print_n_jogo:
 	
 
 loop_matriz_jogo:
-	la	s8, matriz_jogo				# le matriz para jogo
 	beqz	s3, header_jogo
-	lw	a6, (s8)				# carregando matriz de endereços copiada
+	lw	a6, 400, (s8)				# carregando matriz de endereços copiada
 	
 	mv 	a0, a6  				# imprime os valores
 	li 	a7, 11
@@ -274,10 +275,50 @@ valida_tiro:
 	add	s9, s9, a5	
 	
 	lw 	a6, 0(s9)				# pega valor em s9
+	beq 	a6, s5, errou_tiro
+	bgtz	a6, acertou_tiro
 	
-	mv 	a0, a6  				# imprime os valores
-	li 	a7, 11
+acertou_tiro:
+	addi	s10, zero, 400
+	bgt  	a5, s10, fim_error 			#verifica se foi pra posicao errada
+	
+	la	s7, acertou_navio			# le erro
+	mv 	a0, s7  				# imprime os vetor de char
+	li 	a7, 4
 	ecall	
+	
+	add	s3, zero, zero
+	add	t2, zero, zero
+	la	s9, matriz
+	la	s8, matriz_jogo
+	j	procura_navio				#procura e marca todo navio q foi acertado nesse tiro
+
+procura_navio:
+	beq	t2, s5, prepara_loop_matriz
+	addi	t2, t2, 1
+	lw 	s10, 0(s9)				# pega valor em s9
+	addi	s9, s9, 4
+	beq	a6, s10, escreve_na_matriz_jogo
+	j 	procura_navio		
+
+
+prepara_loop_matriz:
+	la	s8, matriz_jogo				# le matriz para jogo
+	j	loop_matriz_jogo
+	
+	
+escreve_na_matriz_jogo:		
+	add	s8, zero, s9				# escrevo valor padrao em todas as 
+	sw	a6, 400,(s8)
+	j 	procura_navio
+errou_tiro:	
+	la	s7, error_tiro				# le erro
+	mv 	a0, s7  				# imprime os vetor de char
+	li 	a7, 4
+	ecall	
+	add	s3, zero, zero
+	j	loop_matriz_jogo
+	  	  		  	  	
 fim_error:
 	la	s7, error				# le erro
 	mv 	a0, s7  				# imprime os vetor de char
